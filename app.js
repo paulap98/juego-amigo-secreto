a181ab1c2efd54500c45ac6300cb19c7b0a328c4
Maria Paula Parra <mariapaulapd1@gmail.com~>
2025-01-28 15:33:25 -0500

    Version agrega

--- app.js
+++ app.js
@@ -20,3 +20,16 @@ function agregarAmigo() {
     actualizarListaAmigos();
 }
 
+function actualizarListaAmigos() {
+    
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li'); 
+        li.textContent = amigos[i]; 
+        lista.appendChild(li);
+        
+    }
+}
+
